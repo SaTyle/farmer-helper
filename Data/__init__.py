232f07b8247b924cc246ce45d763629a7cc6b6e9
SaTyle <zxcvwifi@gmail.com>
2023-07-28 11:25:45 +0530

    __init__

---
+++ Data/__init__.py
@@ -0,0 +1,10 @@
+import os
+# import Flask
+from flask import Flask
+from .config import Config
+# Inject Flask magic
+app = Flask(__name__)
+# load Configuration
+app.config.from_object(Config)
+# Import routing to render the pages
+from app import views
